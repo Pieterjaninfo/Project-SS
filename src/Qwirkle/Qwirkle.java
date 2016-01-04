706e2694960f43e1bdf7c2e55bac0cabc0df309a
Bart <bart@LaptopBdeb>
2016-01-04 21:19:41 +0100

    first test commit

---
+++ src/Qwirkle/Qwirkle.java
@@ -0,0 +1,14 @@
+package Qwirkle;
+
+public class Qwirkle {
+
+	/**
+	 * Startup class.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
