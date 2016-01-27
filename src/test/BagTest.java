0285ec93f597af31a3de327978a3efa81f0c5a17
Bart <bart@LaptopBdeb>
2016-01-27 21:44:05 +0100

    finished bagTest

--- src/test/BagTest.java
+++ src/test/BagTest.java
@@ -2,9 +2,13 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import qwirkle.Bag;
+import qwirkle.Tile;
 
 public class BagTest {
 	private Bag bag;
@@ -23,6 +27,25 @@ public class BagTest {
 	public void sizeAfterStart() {
 		assertEquals(6, bag.giveStartingHand().size());
 		assertEquals(102, bag.getSize());
+	}
+	
+	@Test
+	public void getTilesAmountTest() {
+		int amount = 4;
+		assertEquals(amount, bag.getTiles(amount).size());
+	}
+	
+	@Test
+	public void tradeTilesTest() {
+		List<Tile> hand = new ArrayList<Tile>();
+		hand = bag.giveStartingHand();
+		assertEquals(hand.size(), bag.tradeTiles(hand).size());
+		assertEquals(6, hand.size());
+	}
+	
+	@Test
+	public void getTilesTest() {
+		assertEquals(bag.getSize(), bag.getTiles().size());
 	}
 
 }
