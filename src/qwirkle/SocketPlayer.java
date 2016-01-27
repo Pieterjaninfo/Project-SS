94864fe3e601320726c25e17f17cf848d77b1cb6
Pieter Jan <pieterjaninfo@gmail.com>
2016-01-27 21:36:27 +0100

    layout changes in socketplayer

--- src/qwirkle/SocketPlayer.java
+++ src/qwirkle/SocketPlayer.java
@@ -54,13 +54,7 @@ public class SocketPlayer implements Player {
 				}
 			}
 		}
-		/*
-		for (Tile a : hand) {
-			System.out.println("hand: " + a.toString());
-		}
-		for (Tile b : list) {
-			System.out.println("list: " + b.toString());
-		}*/
+		
 		int[] a = new int[12];
 		for (Tile tile : list) {
 			if (tile.getShape() == Shape.CROSS) {
@@ -150,7 +144,6 @@ public class SocketPlayer implements Player {
 			result += moveTile.getVertPattern().getPoints();
 		}
 		updateScore(result);
-		
 	}
 	
 	/*
